dd55c6118727be5a9983b79c6f01bf5796c229bd
Sophie Obomighie <sobomighie@abuja.jaffatech.net>
2022-07-15 11:41:56 +0100

    Adjusted the structure of the files

---
+++ CSharpImplementation/UnitTest/UnitTest.Tests/FizzBuzzShould.cs
@@ -0,0 +1,13 @@
+using Xunit;
+using 
+namespace UnitTest.Tests
+{
+    public class FizzBuzzShould
+    {
+        [Fact]
+        public void ReturnSpecificValueGivenAnInteger()
+        {
+            Fizzbuz sut = new Fizzbuz();
+        }
+    }
+}
